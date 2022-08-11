1f46da0b5dcb299297e986cb958f5dfdf4991394
mimorphism <bleach707@gmail.com>
2022-08-11 19:56:46 +0800

    refactored Finished mango components

--- src/FinishedMango.js
+++ src/FinishedMango.js
@@ -1,101 +1,104 @@
-import { Card, Image, Text, Modal } from '@mantine/core';
+import { Card, Image, Text, Modal, Indicator, Space } from '@mantine/core';
 import { createStyles } from '@mantine/core';
 import { Group, Button } from '@mantine/core';
 import { useToggle } from '@mantine/hooks';
-import format from 'date-fns/format';
-import parseIso from 'date-fns/parseISO';
-import { FaStar } from 'react-icons/fa';
 import { Fragment } from 'react';
+import { useState } from 'react';
+import DeleteRecordDialog from './DeleteRecordDialog';
+import { RecordType } from './util/utils';
+import { getPrettifiedDate } from './util/utils';
 
-
-
-function getPrettifiedDate(date) {
-  const newDate = parseIso(date, new Date());
-  return format(newDate, 'LLL d yyyy p');
-}
 
 const FinishedMango = ({ mango }) => {
-  const useStyles = createStyles(() => ({
+  const useStyles = createStyles((theme) => ({
     standardFont: {
       // fontFamily: "'Quicksand', sans-serif",
       textTransform: 'uppercase'
-    }
-  }));
+    },
+    indicator: {
+      color: theme.colors.dark[0],
+      fontSize: '20',
+      fontWeight: '700',
+      cursor: 'pointer'
+    },
+    btnGroup:{
+      [`@media (max-width: 1024px)`]: {
+          flexDirection:'column'
+
+      },
+    },
+    optionBtn:{
+      fontSize:theme.fontSizes.xs,
+      width:'100%'
+    },
+    
+  }
+  ));
 
   const { classes } = useStyles();
-  // const [value, setToggle] = useToggle({value:'default',color:'gray'}, [{value:'default',color:'gray'}, {value:'filled',color:'blue'}]);
-  const [valueInfo, setToggleInfo] = useToggle('gray', ['gray', 'dark']);
-  const [valueRemarks, setToggleRemarks] = useToggle('gray', ['gray', 'dark']);
+  const [valueInfo, setToggleInfo] = useToggle(['gray', 'dark']);
+  const [valueRemarks, setToggleRemarks] = useToggle(['gray', 'dark']);
+  const [isDeleteRecordDiagOpen, setDeleteRecordDiagOpen] = useState(false);
 
 
 
   return (
 
-    <div className="mango-details">
-      <div className="container">
-        <div className="row">
-          <Card
-            shadow="sm"
-            style={{ padding: 0 }}>
-            <Card.Section style={{
-              marginBottom: 7,
-              position: 'relative'
-            }}>
-              <Card.Section style={{
-                position: 'absolute',
-                zIndex: 1,
-                // transform: 'translateX(-40%)',
-                left: '22%',
-                // width: 'auto',
-                bottom: 0
-              }}>
-                <Group position='center' spacing='0' noWrap='true'>
-                <Button color={valueInfo} radius="xs" onClick={() => setToggleInfo()}>INFO</Button>
-                <Button color={valueRemarks} radius="xs" onClick={() => setToggleRemarks()}>REMARKS</Button>
-                </Group>
-              </Card.Section >
-
-              <Image src={mango.img} withPlaceholder
-                style={{ padding: '1em' }} />
-            </Card.Section>
-
-            {valueInfo === 'gray' ?
-              <Fragment>
-                <Text style={{
-                  padding: '5px 10px'
-                }}
-                  className={classes.standardFont}
-                  weight={500}
-                  size="xl"
-                  align="center">
-                  {mango.mangoTitle}
-                </Text>
-                <Text className={classes.standardFont}
-                  size="lg"
-                  align="center"
-                  style={{ padding: '10px' }}>
-                  <FaStar style={{ fontSize: '20px' }} /> {mango.author} <FaStar style={{ fontSize: '20px' }} />
-                </Text>
-              </Fragment>
-              :
-              <Fragment><Text style={{
-                padding: '5px'
-              }}
-                className={classes.standardFont}
-                weight={500}
-                size="md"
-                align="center">
-               FINISHED ON: {getPrettifiedDate(mango.completionDateTime)}
-              </Text>
-              </Fragment>
-            }
-            {valueRemarks === 'dark' &&
-             <Modal opened={valueRemarks} onClose={() => setToggleRemarks('gray')} centered withCloseButton={false}>
-             {mango.remarks}
-           </Modal>}
-          </Card>
-        </div>
-      </div>
+    <div>
+      <Indicator label="X"
+        classNames={{ indicator: classes.indicator }}
+        onClick={()=> setDeleteRecordDiagOpen(true)}
+        color="dark" size={20} withBorder>
+      </Indicator>
+      <Card
+        shadow="sm">
+        <Card.Section>
+          <Image src={mango.mango.img} withPlaceholder />
+        </Card.Section>
+        <Space h="xs"></Space>
+        {valueInfo === 'gray' ?
+          <Fragment>
+            <Text
+              weight={800}
+              size="sm"
+              align="center">
+              {mango.mango.mangoTitle}
+            </Text>
+          </Fragment>
+          :
+          
+          <Fragment>
+            <Text className={classes.standardFont}
+              size="sm"
+              align="left"
+              weight={800}>
+              AUTHOR:<Space/>{mango.mango.author}
+            </Text>
+            <Space h="xs"></Space>
+            <Text
+              className={classes.standardFont}
+              weight={800}
+              size="sm"
+              align="left">
+              FINISHED ON:<Space/>{getPrettifiedDate(mango.completionDateTime)}
+            </Text>
+          </Fragment>
+        }
+        {valueRemarks === 'dark' &&
+          <Modal opened={valueRemarks} size="xl" onClose={() => setToggleRemarks('gray')} centered withCloseButton={false}>
+            {mango.remarks ? mango.remarks : 'No remarks'}
+        </Modal>}
+        <Space h="xs="></Space>
+        <Card.Section>
+          <Group className={classes.btnGroup} position='center' spacing='0' noWrap='true' width='100%'>
+            <Button className={classes.optionBtn} color={valueInfo} radius={0} onClick={() => setToggleInfo()}>INFO</Button>
+            <Button className={classes.optionBtn} color={valueRemarks} radius={0} onClick={() => setToggleRemarks()}>REMARKS</Button>
+          </Group>
+        </Card.Section >
+      </Card>
+      <Modal withCloseButton={false} centered opened={isDeleteRecordDiagOpen} onClose={()=> setDeleteRecordDiagOpen(false)} closeOnClickOutside>
+          <DeleteRecordDialog recordType={RecordType.FINISHED}  recordId={mango.finishedId}></DeleteRecordDialog> 
+          </Modal>
     </div>
   );
 
--- src/FinishedReading.js
+++ src/FinishedReading.js
@@ -1,47 +1,74 @@
 import FinishedMango from './FinishedMango';
-import { createStyles, LoadingOverlay } from '@mantine/core';
-import useFetch from './useFetch';
+import { createStyles, LoadingOverlay, Center, Pagination } from '@mantine/core';
+import useAxios from './useAxios';
+import { useState, useEffect } from 'react';
+
+const DEFAULT_RECORDS_PER_PAGE = 20;
+
 
 
 const useStyles = createStyles(() => ({
-    div: {
+  content: {
         display:'grid',
-        gridTemplateColumns: '300px 300px 300px 300px 300px 300px',
-        padding:'5px 47px',
+        justifyContent: 'center',
+        gridTemplateColumns: 'repeat(auto-fit, 190px)',
+        padding: '2rem 2rem',
+        gap: '5em 5em',
+        overflow:'auto',
+        [`@media (max-width: 1024px)`]: {
+            gridTemplateColumns: 'repeat(auto-fill,minmax(120px,1fr))',
+            rowGap: '3em',
+
+        },
+        [`@media (max-width: 395px)`]: {
+            gridTemplateColumns: 'repeat(auto-fill,minmax(120px,1fr))',
+            rowGap: '3em',
+        },
     },
 
-        loader:
-        {
-            size:'xl',
-            variant:'bars'
+  loader:
+  {
+    size: 'xl',
+    variant: 'bars'
+  }
+
+
+}));
+
+const FinishedReading = () => {
+
+  const { classes } = useStyles();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const { data: mangoes, isPending } = useAxios(`/finished?page=${currentPage}&size=${DEFAULT_RECORDS_PER_PAGE}`);
+
+  useEffect(() => {
+        if (mangoes) {
+            if (mangoes.currentPage == 1) {
+                setTotalPages(mangoes.totalPages);
+            }
+            setCurrentPage(mangoes.currentPage);
         }
-    
+    }, [mangoes]);
 
-  }));
-
-const FinishedReading  = () => {
-
-    const { classes } = useStyles();
-    const { data: mangoes, error, isPending } = useFetch('http://localhost:8080/completedMangoes');
-    console.log(mangoes);
-    return (
-        <div>
-            <h1>FINISHED READING</h1>
-        <div className={classes.div}>
-            { error && <div>{ error }</div> }
-            <LoadingOverlay 
+  return (
+      <div>
+         <LoadingOverlay
             loaderProps={{
-                size:'200', variant:'bars'
+              size: '200', variant: 'bars'
 
-            }} visible={isPending}/>
-            { mangoes && 
-            mangoes.map(mango => (
-                <FinishedMango key={mango.mangoId} mango={mango}/>
-      ))}
+            }} visible={isPending} />
+        <div className={classes.content}>
+            {mangoes &&
+              mangoes.content.map(mango => (
+                <FinishedMango key={mango.mangoId} mango={mango} />
+              ))}
+        </div>
+      {totalPages > 1 && <Pagination position="center" align="center" page={currentPage} onChange={setCurrentPage} total={totalPages} />}
       </div>
-      </div>
-      );
-   
+  );
+
 }
- 
+
 export default FinishedReading;
