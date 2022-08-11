4f1ade1c0f34731494169799878f730aae13948d
mimorphism <bleach707@gmail.com>
2022-08-11 18:30:07 +0800

    refactored App.js to accomodate new components and features

--- src/App.js
+++ src/App.js
@@ -1,69 +1,133 @@
 import Header from './Header';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { MantineProvider } from '@mantine/core';
+import Footer from './Footer';
+import { Router, Route, Switch } from 'react-router-dom';
+import { createStyles, MantineProvider, ScrollArea } from '@mantine/core';
 import UpdateMango from './UpdateMango';
-import { Global } from '@mantine/core';
 import CurrentlyReading from './CurrentlyReading';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
-} from "@apollo/client";
 import FinishedReading from './FinishedReading';
+import SearchMango from './SearchMango';
+import { apolloClientInstance } from './apolloClientInstance';
+import { ApolloProvider } from "@apollo/client";
+import Login from './Login';
+import { useReducer, useEffect } from 'react';
+import { UserContext } from './services/UserContext';
+import history from './history';
+import Backlog from './Backlog';
+import { Helmet } from "react-helmet";
+import { NotificationsProvider } from '@mantine/notifications';
+import GlobalStyles from './GlobalStyles';
+import { useMediaQuery } from '@mantine/hooks';
 
-const client = new ApolloClient({
-  uri: 'https://graphql.anilist.co',
-  cache: new InMemoryCache()
-});
+
 
 
 function App() {
 
+
+  const initialState =
+  {
+    loggedin: false,
+    username: ""
+  }
+
+  const loginReducer = (state, action) => {
+    switch (action.type) {
+      case "LOGGED_IN":
+        return { loggedin: true, username: action.value };
+      case "LOGGED_OUT":
+        return { loggedin: false, username: "" }
+      default:
+        return initialState;
+    }
+  }
+
+
+  const [state, dispatch] = useReducer(loginReducer, initialState);
+
+
+
+  const useStyles = createStyles(() => ({
+    content: {
+      flex:'1 1 auto',
+      maxWidth: '1440px',
+      width: `calc(100vw - 20vw)`,
+      height: 'calc(100vh - 18vh)',
+      margin: `0 auto`,
+      [`@media (max-width: 1024px)`]: {
+        height: 'calc(100vh - 5vh)',
+
+      },
+
+    },
+
+    main: {
+      // overflow: 'hidden',
+      display:'flex',
+      flexDirection:'column',
+      height:'100%',
+    }
+
+  }
+  ));
+
+  const { classes } = useStyles();
+  const matchesMobileView = useMediaQuery('(min-width: 1024px)');
+
+
+
+
+  useEffect(() => {
+    if (localStorage.getItem('user')) {
+      dispatch({ type: 'LOGGED_IN', value: JSON.parse(localStorage.getItem('user')).user });
+    }
+  }, [])
+
+
   return (
     <MantineProvider theme={{
       colorScheme: 'dark',
-      fontFamily: 'Quicksand, sans-serif'
-      }}
-      >
-        <Global
-        styles={(theme) =>({
-          fontFamily: 'Quicksand, sans-serif',
-          h1:
-          {
-            textAlign: 'center',
-            fontSize: '48px',
-            fontWeight: 800,
-            display:'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            position:'relative',
-            top: `2.5em`,
-          }
-        })}
-        >
-        </Global>
-    <Router>
-    <div className="App">
-      <Header/>
-      <div className="content">
-        <Switch>
-        <ApolloProvider client={client}>
-          <Route path = "/currentlyreading">
-          <CurrentlyReading/>
-          </Route>
-          <Route path = "/finishedreading">
-          <FinishedReading/>
-          </Route>
-          <Route path = "/updatemango">
-          <UpdateMango/>
-          </Route>
-          </ApolloProvider>
-        </Switch>
-        </div>
-    </div>
-    </Router>
+      fontFamily: 'Quicksand, sans-serif',
+    }
+    }
+    >
+      <NotificationsProvider autoClose={4000}>
+        <GlobalStyles />
+        <Helmet>
+          <title>MangoTracko</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        </Helmet>
+        <Router history={history}>
+          <div className={!state.loggedin ? "main" : classes.main}>
+            <UserContext.Provider value={{ state, dispatch }}>
+              {!state.loggedin ? <Login /> : <>
+                <Header username={state.username} />
+                <ScrollArea offsetScrollbars scrollbarSize={4} className={classes.content}>
+                  <Switch>
+                    <ApolloProvider client={apolloClientInstance}>
+                      <Route exact path="/currentlyreading">
+                        <CurrentlyReading />
+                      </Route>
+                      <Route exact path="/finished">
+                        <FinishedReading />
+                      </Route>
+                      <Route exact path="/updatemango">
+                        <UpdateMango />
+                      </Route>
+                      <Route exact path="/addmango">
+                        <SearchMango />
+                      </Route>
+                      <Route exact path="/backlog">
+                        <Backlog />
+                      </Route>
+                    </ApolloProvider>
+                  </Switch>
+                </ScrollArea>
+            {matchesMobileView && <Footer/>}
+            </>}
+          </UserContext.Provider>
+          </div>
+        </Router>
+      </NotificationsProvider>
     </MantineProvider>
   );
 }
