ba6a61f518bdb514a2765af13763e8fa4410f0b3
mimorphism <bleach707@gmail.com>
2022-08-28 16:32:13 +0800

    refactored LogoutButton to UserButton for inclusion of option to go to user profile page

---
+++ src/UserButton.js
@@ -0,0 +1,46 @@
+import { Menu, createStyles, UnstyledButton} from '@mantine/core';
+import { FaUserAstronaut } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+
+const UserButton = ({ username, tryLogout }) => {
+
+  const useStyles = createStyles((theme, _params, getRef) => ({
+
+    texts: {
+      fontSize: theme.fontSizes.sm,
+      fontWeight: '800',
+      [`@media (max-width: 1024px)`]: {
+        fontSize: theme.fontSizes.sm,
+      },
+    },
+  }));
+
+  const { classes } = useStyles();
+
+  return (
+    <Menu position="bottom" 
+      radius={0} size="xl" className={classes.texts}
+      trigger="hover" openDelay={100} closeDelay={100}>
+      <Menu.Target>
+        <div className='headerLinkWrapper'>
+          <UnstyledButton
+          ><FaUserAstronaut
+              color="white" size="2.5rem" /></UnstyledButton>
+        </div>
+      </Menu.Target>
+      <Menu.Dropdown>
+        <Menu.Label className={classes.texts}>User: {username}</Menu.Label>
+        <Menu.Divider />
+        <Menu.Item className={classes.texts} component={Link} to="home">Profile</Menu.Item>
+        <Menu.Item
+          color="red" onClick={() => tryLogout()}
+          className={classes.texts}
+        >LOGOUT</Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
+  );
+}
+
+
+export default UserButton;
