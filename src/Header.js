2b69f5ee2ba1c5e3489725c1b7bc06f4fe0e23a1
mimorphism <bleach707@gmail.com>
2022-08-30 17:58:05 +0800

    corrected searchmango route name
    refactored tryLogout to call reducer dispatch for logout

--- src/Header.js
+++ src/Header.js
@@ -6,9 +6,10 @@ import AuthService from './services/AuthService';
 import { useLocation } from "react-router-dom";
 import { useMediaQuery } from '@mantine/hooks';
 import MobileMenu from "./MobileMenu";
-import {FaSearchengin} from 'react-icons/fa';
-import LogoutButton from './LogoutButton';
-
+import { FaSearchengin } from 'react-icons/fa';
+import UserButton from "./UserButton";
+import { notifyLoadingCallback } from "./util/utils";
+import TokenService from "./services/TokenService";
 
 
 
@@ -19,15 +20,15 @@ const Header = ({ username }) => {
   const currentRoute = useLocation().pathname;
 
   const toggleActivePage = (state, action) => {
-    switch(action.type){
+    switch (action.type) {
       case "BACKLOG":
-        return { BACKLOG: true, CTLY_READING: false,FINISHED: false, ADD:false};
+        return { BACKLOG: true, CTLY_READING: false, FINISHED: false, ADD: false };
       case "CTLY_READING":
-        return { BACKLOG: false, CTLY_READING: true,FINISHED: false, ADD:false};
+        return { BACKLOG: false, CTLY_READING: true, FINISHED: false, ADD: false };
       case "FINISHED":
-        return { BACKLOG: false, CTLY_READING: false,FINISHED: true, ADD:false};
+        return { BACKLOG: false, CTLY_READING: false, FINISHED: true, ADD: false };
       case "ADD":
-        return { BACKLOG: false, CTLY_READING: false,FINISHED: false, ADD:true};
+        return { BACKLOG: false, CTLY_READING: false, FINISHED: false, ADD: true };
       default:
         return initialState;
     }
@@ -38,27 +39,27 @@ const Header = ({ username }) => {
     BACKLOG: false,
     CTLY_READING: false,
     FINISHED: false,
-    ADD:false
+    ADD: false
   }
 
 
 
-const [activePage, toggle] = useReducer(toggleActivePage, initialActiveState); 
+  const [activePage, toggle] = useReducer(toggleActivePage, initialActiveState);
 
- useEffect(() => {
-   if(currentRoute === '/backlog' ){
-     toggle({ type: 'BACKLOG', value: true});
-   }
-   else if(currentRoute === '/finishedreading' ){
-     toggle({ type: 'FINISHED', value: true});
-   }
-   else if(currentRoute === '/currentlyreading' ){
-     toggle({ type: 'CTLY_READING', value: true});
-   }
-   else if(currentRoute === '/addMango' ){
-     toggle({ type: 'ADD', value: true});
-   }
-    
+  useEffect(() => {
+    if (currentRoute === '/backlog') {
+      toggle({ type: 'BACKLOG', value: true });
+    }
+    else if (currentRoute === '/finishedreading') {
+      toggle({ type: 'FINISHED', value: true });
+    }
+    else if (currentRoute === '/currentlyreading') {
+      toggle({ type: 'CTLY_READING', value: true });
+    }
+    else if (currentRoute === '/addmango') {
+      toggle({ type: 'ADD', value: true });
+    }
+
   }, []);
 
 
@@ -66,16 +67,16 @@ const [activePage, toggle] = useReducer(toggleActivePage, initialActiveState);
   const useStyles = createStyles((theme, _params, getRef) => ({
 
     header: {
-      
+
       ref: getRef('header'),
       display: 'flex',
       textAlign: 'right',
       lineHeight: '40px',
       padding: '0 30px',
-      position:`relative`,
-      flex:'0 1 auto',
+      position: `relative`,
+      flex: '0 1 auto',
       borderBottom: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]}`,
-      
+
       // '&:before': {
       //   content: '""',
       //   //https://i.imgur.com/CWyDX0e.png - alice
@@ -96,8 +97,8 @@ const [activePage, toggle] = useReducer(toggleActivePage, initialActiveState);
       // },
 
       [`@media (max-width: 1024px)`]: {
-        padding:0,
-        backgroundColor:theme.colors.dark[4],
+        padding: 0,
+        backgroundColor: theme.colors.dark[4],
         borderBottom: 'none',
         justifyContent: 'center',
         '&:before': {
@@ -120,7 +121,7 @@ const [activePage, toggle] = useReducer(toggleActivePage, initialActiveState);
       fontWeight: 800,
       display: 'flex',
       justifyContent: 'center',
-      
+
 
       [`&:hover .${getRef('logoBar')}`]: {
         opacity: 1,
@@ -128,7 +129,7 @@ const [activePage, toggle] = useReducer(toggleActivePage, initialActiveState);
       }
 
     },
-    
+
 
     operationsList:
     {
@@ -136,8 +137,8 @@ const [activePage, toggle] = useReducer(toggleActivePage, initialActiveState);
       width: '100%',
       display: 'flex',
       justifyContent: 'center',
-      padding:0,
-      margin:0
+      padding: 0,
+      margin: 0
     },
 
 
@@ -156,66 +157,65 @@ const [activePage, toggle] = useReducer(toggleActivePage, initialActiveState);
 
     linkBarActive:
     {
-        opacity:1
+      opacity: 1
     },
-    
-    container:{
-        display: 'flex',
-        justifyContent: 'space-between',
-        paddingBottom: theme.spacing.xs,
+
+    container: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      paddingBottom: theme.spacing.xs,
     }
   }));
 
   const { classes, cx } = useStyles();
   const matches = useMediaQuery('(min-width: 1025px)');
-  const matches1368 = useMediaQuery('(min-width: 1368px)');
-  const [logoutBtnContent, setLogoutBtnContent] = useState(username);
 
 
 
   const tryLogout = () => {
-    AuthService.logout().then((logout) => {
-      dispatch({ type: 'LOGGED_OUT' });
-      console.log("logout sukses?: " + logout.message);
-    });
+    AuthService.logout().then(() => {
+      TokenService.removeUser();
+      notifyLoadingCallback('See you again '+ username, 'logout','Logout succesful!', false );
+  });
+  dispatch({ type: 'LOGGED_OUT' });
 
-  }
+};
 
-  return (
-      <div className={classes.header}>
-      {!matches && <MobileMenu username={username} tryLogout={tryLogout}></MobileMenu>}
-         {!matches &&<Text align="left" className='logoOnly'>MANGOTRACKO</Text>}
-      
-        {matches && 
-        <Container className={classes.container}>    
+return (
+  <div className={classes.header}>
+    {!matches && <MobileMenu username={username} tryLogout={tryLogout}></MobileMenu>}
+    {!matches && <Text align="left" className='logoOnly'>MANGOTRACKO</Text>}
+
+    {matches &&
+      <Container className={classes.container}>
         <div className={classes.operationsList}>
-        <Link to="addMango" className='headerLinkWrapper'
-          onClick={() => toggle({ type: 'ADD', value: true})}
-          ><UnstyledButton><FaSearchengin color="white" size="2.5rem"/></UnstyledButton>
-            <div className={cx(classes.linkBar, { [classes.linkBarActive]: activePage.ADD})}></div>
-            </Link>
+          <Link to="addMango" className='headerLinkWrapper'
+            onClick={() => toggle({ type: 'ADD', value: true })}
+          ><UnstyledButton><FaSearchengin color="white" size="2.5rem" /></UnstyledButton>
+            <div className={cx(classes.linkBar, { [classes.linkBarActive]: activePage.ADD })}></div>
+          </Link>
           <Link to="backlog" className='headerLinkWrapper'
-          onClick={() => toggle({ type: 'BACKLOG', value: true})}
+            onClick={() => toggle({ type: 'BACKLOG', value: true })}
           >Backlog
-            <div className={cx(classes.linkBar, { [classes.linkBarActive]: activePage.BACKLOG})}></div>
-            </Link>
+            <div className={cx(classes.linkBar, { [classes.linkBarActive]: activePage.BACKLOG })}></div>
+          </Link>
           <Link to="finished" className='headerLinkWrapper'
-           onClick={() => toggle({ type: 'FINISHED', value: true})}
-           >Finished
-            <div className={cx(classes.linkBar, { [classes.linkBarActive]: activePage.FINISHED})}></div>
-            </Link>
+            onClick={() => toggle({ type: 'FINISHED', value: true })}
+          >Finished
+            <div className={cx(classes.linkBar, { [classes.linkBarActive]: activePage.FINISHED })}></div>
+          </Link>
           <Link to="currentlyreading" className='headerLinkWrapper'
-          onClick={() => toggle({ type: 'CTLY_READING', value: true})}
+            onClick={() => toggle({ type: 'CTLY_READING', value: true })}
           >Currently Reading
-            <div className={cx(classes.linkBar, { [classes.linkBarActive]: activePage.CTLY_READING})}></div>
-            </Link>
-          <LogoutButton username={username} tryLogout={tryLogout}/>
-          </div>
-          </Container>
-          }
-        
-      </div>
-  );
+            <div className={cx(classes.linkBar, { [classes.linkBarActive]: activePage.CTLY_READING })}></div>
+          </Link>
+          <UserButton username={username} tryLogout={tryLogout} />
+        </div>
+      </Container>
+    }
+
+  </div>
+);
 }
 
 export default Header;
