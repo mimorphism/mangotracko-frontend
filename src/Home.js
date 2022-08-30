30f8dc789f9eb2b4835ed2127a00da5742899f70
mimorphism <bleach707@gmail.com>
2022-08-30 18:17:23 +0800

    removed loading notification and replaced with dynamic greeting text

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,7 @@
 import GeneralStats from "./GeneralStats";
-import { Text, Space, Stack, LoadingOverlay } from "@mantine/core";
+import { Text, Stack, LoadingOverlay } from "@mantine/core";
 import TokenService from "./services/TokenService";
 import useAxios from './useAxios';
-import { notifyLoading } from "./util/utils";
-import { hideNotification } from "@mantine/notifications";
 import MostRecentUpdatesAndDailyStats from "./MostRecentUpdatesAndDailyStats";
 
 
@@ -12,15 +10,13 @@ const Home = () => {
      const { data: stats, isPending } = useAxios('/userstats');
      return (
           <Stack spacing={10}>
+          <Text weight={700} size="xl">{isPending ? `Populating your stats...`:`Hey there ${TokenService.getUsername()}!`}</Text>
                <LoadingOverlay
                     loaderProps={{
                          size: '200', variant: 'bars'
 
                     }} visible={isPending}/>
                
-               {isPending && notifyLoading('Populating your stats...', 'user-stats')}
-               {!isPending && hideNotification('user-stats')}
-               <Text weight={700} size="xl">Hey there {TokenService.getUsername()}!</Text>
                {stats && <GeneralStats stats={stats.generalStats}/>}
                {stats &&<MostRecentUpdatesAndDailyStats mostRecentUpdateStats={stats.mostRecentUpdateStats} dailyStats={stats.dailyStats}></MostRecentUpdatesAndDailyStats>}
           </Stack>
